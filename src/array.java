07a1f35bb78097781e913a9d5903e042a7afec5d
Cube-Box-391 <adrian.duqueluengo@gmail.com>
2025-11-03 17:18:41 +0100

    Ejercicio Batman Terminado

---
+++ src/array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[] args) {
+        double[] notas = {8.6,5.1,10.0,9.8,7.5};
+        System.out.print("Nota 2: " + notas[2]);
+        double media, suma=0.0;
+
+        for(int i=0; i<notas.length; i++){
+            System.out.println(notas[i]);
+            suma = suma + notas[i];
+        }
+        media = suma/notas.length;
+        System.out.println("Media: " + media);
+    }
+}
---
+++ src/ej13.java
@@ -0,0 +1,20 @@
+public class ej13 {
+    public static void main(String[] args) {
+//        ejercicio batman
+
+//         Velocidad inicial (50 km/h -> m/s)
+        double velocidad = 50 * 1000.0 / 3600.0; // 13.8889 m/s
+        double distancia = 0.0;
+        int segundos = 0;
+
+//        Bucle que reduce la velocidad un 20% cada segundo
+        while (velocidad >= 0.5) {
+            distancia += velocidad;  // Avanza durante ese segundo
+            velocidad *= 0.8;         // Reduce un 20%
+            segundos++;
+        }
+
+        System.out.println("El Batmóvil se detiene en " + segundos + " segundos.");
+        System.out.printf("La cueva debe tener al menos %.2f metros de profundidad.%n", distancia);
+    }
+}
